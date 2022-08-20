e8fe2af1034c27a54112023c50408c6c5fe35627
Yuliya Sidarenka <yulka1102@gmail.com>
2022-08-20 16:14:07 +0200

    getByText

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-//
-// test('render link', () => {
-//   const { getByText } = render(<App/>);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// })
-
-test('show DOM in console with screen', () => {
-  render(<App/>);
-  screen.debug()
-})
-
-test('use fragment', () => {
-  const { asFragment } = render(<App/>);
-  expect(asFragment(<App/>)).toMatchSnapshot();
+describe("App", () => {
+    it("renders App component", () => {
+        render(<App/>);
+        screen.debug();
+        expect(screen.getByText("Search:")).toBeInTheDocument()
+    })
 })
